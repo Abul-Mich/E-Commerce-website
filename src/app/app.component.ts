4071f0d8a653f731a1943dc9b1ff727fdc699849
michel geha <micheldg2009(@gmail.com>
2026-03-11 02:43:49 +0200

    Final version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   template: ` <router-outlet /> `,
+  styleUrl: './app.scss',
 })
 export class AppComponent {
   title = 'angular-app';
